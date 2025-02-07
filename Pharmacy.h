3f30897656f1fdc238917275eea50af3174c2c17
lapshuntanya <lapshun.tatiana@gmail.com>
2025-02-07 18:52:46 +0200

    class Pharmacy

---
+++ Pharmacy.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Medicine.h"
+#include "Array_Template.h"
+
+class Pharmacy
+{
+	char* m_name;
+	int m_sizeMedicine;
+	Medicine* m_arr;
+
+public:
+	Pharmacy();
+	~Pharmacy();
+
+	void setName(const char* name);
+	const char* getName()const;
+
+	int getAmoutofMedicine()const;
+	void addMedicine(Medicine object);
+
+};
+
