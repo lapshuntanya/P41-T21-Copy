9d897e90b7a05129aa7d20614eec0fee0af300a9
lapshuntanya <lapshun.tatiana@gmail.com>
2025-02-07 19:21:30 +0200

    main

--- P41-T21-Copy.cpp
+++ P41-T21-Copy.cpp
@@ -1,13 +1,17 @@
-#include "Medicine.h"
+#include "Pharmacy.h"
 
 int main()
 {
-   /* Medicine obj("Aspiryn", "pills", 45.99, "Ukraine");
-    obj.setTitle("Aspiryn Forte");
-    obj.showInfo();
 
 
-    cout << "\n\n\n";
+
+
+    Medicine obj1("Aspiryn", "pills", 45.99, "Ukraine");
+    obj1.setTitle("Aspiryn Forte");
+    /* obj.showInfo();
+
+
+     cout << "\n\n\n";
     Medicine obj2 = obj;  //Конструктор копіювання => this-obj2,    copy-obj
     obj2.setCountry("India");
     obj2.setTitle("Noshpa");
@@ -20,6 +24,13 @@ int main()
     obj.showInfo();
     obj2.showInfo();*/
 
+    Pharmacy pharm;
+    pharm.setName("Pharmacy-911");
+
+    pharm.addMedicine(obj1);//Copy constructor
+    pharm.addMedicine(Medicine("Nurofen", "pills", 230, "France"));
+
+    pharm.showInfo();
 
 }
 
