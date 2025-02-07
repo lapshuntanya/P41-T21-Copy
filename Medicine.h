3d55022924dccaaaa9d09838feb7fddb06d00325
lapshuntanya <lapshun.tatiana@gmail.com>
2025-02-07 17:26:19 +0200

    class Medicine

--- Medicine.h
+++ Medicine.h
@@ -5,5 +5,25 @@ using namespace std;
 
 class Medicine
 {
+	char* m_title;
+	char* m_type;
+	float m_price;
+	char* m_country;
+public:
+	Medicine();
+	Medicine(const char* title, const char* type, float price, const char* country);
+	~Medicine();
+
+	void setTitle(const char* title);
+	void setType(const char* type);
+	void setPrice(float price);
+	void setCountry(const char* country);
+
+	const char* getTitle()const;
+	const char* getType()const;
+	float getPrice()const;
+	const char* getCountry()const;
+
+	void showInfo()const;
 };
 
